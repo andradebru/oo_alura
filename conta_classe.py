104570ff2ca0845685d5f1e06e93b542abd43a42
andradebru <brunansandrade@gmail.com>
2023-03-07 11:06:50 -0300

    implementa saca e deposita

--- conta_classe.py
+++ conta_classe.py
@@ -5,12 +5,19 @@ class Conta:
         self.titular = titular
         self.saldo = saldo
         self.limite = limite
+        # constroi a conta
         # qdo tem um valor padrao pode-se passar no argumento da classe em vez de na criacao do objeto 
 
     def extrato(self):
-        print(f"Titular: {self.titular} // Saldo: {self.saldo}")
+        print(f'Titular: {self.titular} // Saldo: {self.saldo}')
         # from conta_classe import Conta
-        # conta1 = Conta(1, "Bruna", 100, 2000)
+        # conta1 = Conta(1, 'Bruna', 100, 2000)
         # conta1.extrato()
 
+    def deposita(self, valor):
+        self.saldo += valor
+        print(f'Voce depositou R$ {valor} e agora seu saldo é de R$ {self.saldo}')
     
+    def saca(self, valor):
+        self.saldo -= valor
+        print(f'Voce depositou R$ {valor} e agora seu saldo é de R$ {self.saldo}')
