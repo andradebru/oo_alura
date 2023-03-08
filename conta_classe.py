49b910438217fc2036b79abebf60e4cb9e97fb9f
andradebru <brunansandrade@gmail.com>
2023-03-08 10:16:50 -0300

    add comentario explicativo

--- conta_classe.py
+++ conta_classe.py
@@ -1,7 +1,7 @@
 class Conta:
     def __init__(self, numero, titular, saldo, limite=1000): 
         print(f'Construindo objeto {self}')
-        self.__numero = numero # assim como no java se usa a palavra private p limitar a classe, em python usamos 2 underscores '__'
+        self.__numero = numero # assim como no java se usa a palavra private p limitar a classe, em python usamos 2 underscores no inicio '__'
         self.__titular = titular
         self.__saldo = saldo
         self.__limite = limite
@@ -21,3 +21,6 @@ class Conta:
     def saca(self, valor):
         self.__saldo -= valor
         print(f'Voce depositou R$ {valor} e agora seu saldo é de R$ {self.__saldo}')
+
+    # agora com os __ antes do atributo, eles seriam chamados no terminal como '_Classe__atributo', indicando q sao privados 
+    # e deveriam ser modificados atraves dos metodos
