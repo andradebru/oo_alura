e00e3dcfac8fbc2a7dd5c382751e3230052a6cbc
andradebru <brunansandrade@gmail.com>
2023-03-08 10:11:34 -0300

    deixa atributos privados

--- conta_classe.py
+++ conta_classe.py
@@ -1,23 +1,23 @@
 class Conta:
     def __init__(self, numero, titular, saldo, limite=1000): 
         print(f'Construindo objeto {self}')
-        self.numero = numero
-        self.titular = titular
-        self.saldo = saldo
-        self.limite = limite
+        self.__numero = numero # assim como no java se usa a palavra private p limitar a classe, em python usamos 2 underscores '__'
+        self.__titular = titular
+        self.__saldo = saldo
+        self.__limite = limite
         # constroi a conta
         # qdo tem um valor padrao pode-se passar no argumento da classe em vez de na criacao do objeto 
 
     def extrato(self):
-        print(f'Titular: {self.titular} // Saldo: {self.saldo}')
+        print(f'Titular: {self.__titular} // Saldo: {self.__saldo}')
         # from conta_classe import Conta
         # conta1 = Conta(1, 'Bruna', 100, 2000)
         # conta1.extrato()
 
     def deposita(self, valor):
-        self.saldo += valor
-        print(f'Voce depositou R$ {valor} e agora seu saldo é de R$ {self.saldo}')
+        self.__saldo += valor
+        print(f'Voce depositou R$ {valor} e agora seu saldo é de R$ {self.__saldo}')
     
     def saca(self, valor):
-        self.saldo -= valor
-        print(f'Voce depositou R$ {valor} e agora seu saldo é de R$ {self.saldo}')
+        self.__saldo -= valor
+        print(f'Voce depositou R$ {valor} e agora seu saldo é de R$ {self.__saldo}')
